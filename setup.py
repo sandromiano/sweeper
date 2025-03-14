6948d691ec83d0170b69940515f0bee8ee426fd8
sandromiano <sandro.miano@yale.edu>
2025-03-14 02:43:22 -0400

    few changes

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+      name = 'Sweeper',
+      version = '0.0.1',
+      author = 'Alessandro Miano',
+      author_email = 'superconducting.nina@gmail.com',
+      description = ('Sweeper, a python package'
+                      ' to perform, save and plot n-dimensional sweeps.'),
+      license = 'GNU General Public License, version 2',
+      packages = ['sweeper'],
+      package_data = {},
+      include_package_data = True,
+      install_requires = []
+      )
--- sweeper/colormaps.py
+++ sweeper/colormaps.py
@@ -1,11 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import colors as plt_colors
+from matplotlib import colormaps
 
 def define_phaseColorMap():
     # all numbers from Igor wave 'phaseindex'
     # Igor colors take RGB from 0 to 65535
-    rgb = np.zeros((360,3), dtype=np.float)
+    rgb = np.zeros((360,3), dtype=np.float64)
     rgb[0:90,0] = np.arange(0, 63000, 700)
     rgb[90:180, 0] = 63000 * np.ones(90)
     rgb[180:270, 0] = np.arange(63000, 0, -700)
@@ -15,6 +16,6 @@ def define_phaseColorMap():
     rgb = rgb  / 65535.0
     # ListedColormap takes an arry of RGB weights normalized to be in [0,1]
     phase_cmap = plt_colors.ListedColormap(rgb, name='phase')
-    plt.register_cmap(name='phase', cmap=phase_cmap) 
+    colormaps.register(phase_cmap, name='phase') 
     
 define_phaseColorMap()
